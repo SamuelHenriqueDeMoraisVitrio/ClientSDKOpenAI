313cdaef1c8766289516a4cb3596af5830402db1
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-12 03:04:44 -0300

    att

--- src/OpenAi/response/def.response.c
+++ src/OpenAi/response/def.response.c
@@ -20,3 +20,51 @@ cJSON * OpenAiInterface_get_response_cJSON_raw(OpenAiInterface *self, long index
     return cJSON_GetArrayItem(self->response_array, index);
 
 }
+
+bool OpenAiInterface_error(OpenAiInterface *self, long index){
+    cJSON *response = OpenAiInterface_get_response_cJSON_raw(self, index);
+    if(response == NULL){
+        return true;
+    }
+    cJSON *error = cJSON_GetObjectItem(response, "error");
+    if(error == NULL){
+        return false;
+    }
+    return true;
+}
+
+char *OpenAiInterface_get_error_message(OpenAiInterface *self, long index){
+    cJSON *response = OpenAiInterface_get_response_cJSON_raw(self, index);
+    if(response == NULL){
+        return NULL;
+    }
+    cJSON *error = cJSON_GetObjectItem(response, "error");
+    if(error == NULL){
+        return NULL;
+    }
+    cJSON *message = cJSON_GetObjectItem(error, "message");
+    if(message == NULL){
+        return NULL;
+    }
+    return message->valuestring;
+}
+
+char * OpenAiInterface_get_response_content(OpenAiInterface *self, long index,long choice){
+    cJSON *response = OpenAiInterface_get_response_cJSON_raw(self, index);
+    if(response == NULL){
+        return NULL;
+    }
+    cJSON *choices = cJSON_GetObjectItem(response, "choices");
+    if(choices == NULL){
+        return NULL;
+    }
+    cJSON *choice_item = cJSON_GetArrayItem(choices, choice);
+    if(choice_item == NULL){
+        return NULL;
+    }
+    cJSON *text = cJSON_GetObjectItem(choice_item, "content");
+    if(text == NULL){
+        return NULL;
+    }
+    return text->valuestring;
+}
