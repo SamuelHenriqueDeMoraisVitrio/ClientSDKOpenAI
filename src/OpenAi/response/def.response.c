e3197d487d7d727098f7e0ea15767b69230f8d7a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-12 02:48:40 -0300

    att

---
+++ src/OpenAi/response/def.response.c
@@ -0,0 +1,20 @@
+//silver_chain_scope_start
+//DONT MODIFY THIS COMMENT
+//this import is computationally generated
+//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
+#include "../../imports/imports.dec.h"
+//silver_chain_scope_end
+
+cJSON * OpenAiInterface_get_response(OpenAiInterface *self, long index){
+    int size = cJSON_GetArraySize(self->response_array);
+    //if index its -1 than its last
+    if(index < 0){
+        index = size - index;
+    }
+    if(index < 0 || index >= size){
+        return NULL;
+    }
+
+    return cJSON_GetArrayItem(self->response_array, index);
+
+}
